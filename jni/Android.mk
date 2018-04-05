12c96585992db824c87938fc4db4233960616c36
Aaron Kling <webgeek1234@gmail.com>
2018-04-05 13:13:06 -0500

    Refactor android jni makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,29 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := retro
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-CORE_DIR	= ..
+CORE_DIR := $(LOCAL_PATH)/..
 
 include $(CORE_DIR)/Makefile.common
 
-LOCAL_SRC_FILES    += $(SOURCES_C)
-LOCAL_CFLAGS += -O2 -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 -DNDEBUG=1 -DINLINE="inline" $(INCFLAGS)
+COREFLAGS := -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 -DINLINE="inline" $(INCFLAGS)
 
-LOCAL_LDLIBS += -lz -llog
-
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+LOCAL_LDLIBS    := -lz -llog
 include $(BUILD_SHARED_LIBRARY)
