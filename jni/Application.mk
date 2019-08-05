6c50e6c19062414a52e71d94d745d4dc5ac918b4
David Skywalker <dantoine@gmail.com>
2019-08-05 17:03:40 +0200

    Changing Android optimizations to -Ofast, thx to @rsn8887 to point me.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,8 @@
 APP_ABI := all
-APP_CFLAGS += -O0
+# Without the -Ofast here, the core crashes on armv7a devices when opening the on-screen keyboard with L.
+# The crash is due to some unaligned memory access (BUS_ADRALN) and doesn't happen on other platforms.
+# Building the core with the buildbot running NDK v20 on linux with default flags caused the crash.
+# Building the core with NDK v20 on Darwin must use different default flags or do something else different
+# because the crash didn't happen with those builds, and the resulting .so was also ~700 kb larger in size.
+# This should be investigated more in the future.
+APP_CFLAGS += -Wno-error=format-security -Ofast
